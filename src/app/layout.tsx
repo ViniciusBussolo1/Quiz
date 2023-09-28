4e4b72c2dd21be3508bc6340c2c18a5fd33d225f
Vinicius Bussolo <vinicius.buss13@gmail.com>
2023-09-28 10:29:45 -0300

    :sparkles: feat: config react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { queryClient } from '@/services/queryClient'
+import { QueryClientProvider } from '@tanstack/react-query'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </body>
     </html>
   )
 }
