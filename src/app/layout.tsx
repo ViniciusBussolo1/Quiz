9f28990a4c682cd12e4b3649da6ea6b60128e11d
Vinicius Bussolo <vinicius.buss13@gmail.com>
2023-09-28 11:11:09 -0300

    :recycle: refector: refector config ReactQueryProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { queryClient } from '@/services/queryClient'
-import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryProvider } from './ReactQueryProvider'
 
 import './globals.css'
 
@@ -18,12 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ReactQueryProvider>
   )
 }
